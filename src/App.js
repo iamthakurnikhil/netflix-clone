056bbd73d4dd7ea688f601b39a5efb7c2ad0cb83
iamthakurnikhil <iamthakurnikhil@hotmail.com>
2022-09-08 17:11:05 +0530

    some more work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
+import Account from "./pages/Account";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
+
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/account" element={<Account/>}></Route>
       </Routes>
+    </AuthContextProvider>
     </>
   );
 }
